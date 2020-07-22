53fc6f4fce857ca6bcc6c58b18432f433c86de09
Soma A. Hota <shnasabrainwave@gmail.com>
2020-07-22 12:56:40 -0400

    Update gdrive.py

--- gdrive.py
+++ gdrive.py
@@ -3,5 +3,5 @@ import time
 
 url ='https://drive.google.com/uc?id=1ENUnOgRwcPoigIS8bwZsWt9Y4zH4MZmH'
 output='VGG16_ImageNet.pt'
-time.sleep(1000)
+time.sleep(200)
 gdown.download(url, output, quiet=False)
